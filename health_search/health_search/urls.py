381dc22a0222ef7945c165e99f31067f29de0ec1
thekevinchi <thekevinchi@gmail.com>
2016-03-08 23:17:18 +0000

    added favicon +small changes to seetong and urls

--- health_search/health_search/urls.py
+++ health_search/health_search/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'health_search.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from registration.backends.simple.views import RegistrationView
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/eHealth/'
+
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'health_search.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'eHealth^/', include('eHealth.urls')),
+                       url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+                       url(r'^accounts/', include('registration.backends.simple.urls')),
+                       )
