3a67db0655c3cae2a84a0d49046b0deee96a7959
thekevinchi <thekevinchi@gmail.com>
2016-03-08 23:32:34 +0000

    small fix

--- health_search/eHealth/views.py
+++ health_search/eHealth/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from health_search.settings import STATIC_PATH
+from datetime import datetime
+
 
 # Create your views here.
--- health_search/health_search/urls.py
+++ health_search/health_search/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 from registration.backends.simple.views import RegistrationView
 
 class MyRegistrationView(RegistrationView):
