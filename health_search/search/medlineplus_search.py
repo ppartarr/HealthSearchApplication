932f9848ca887bd13b77853382d46e7eb75dc5c0
2142915 <ppartarr@outlook.com>
2016-03-11 16:55:13 +0000

    added eHealth to INSTALLED_APPS in seetings.py

--- health_search/search/medlineplus_search.py
+++ health_search/search/medlineplus_search.py
@@ -10,16 +10,18 @@ def medlineplus_run_query(search_terms):
     query = search_terms.replace(" ", "+")
     search_url = "{0}{1}".format(
         root_url,
-        search_url
+        query,
     )
 
     results=[]
     print search_url
     try:
         response = urllib2.urlopen(search_url).read()
-        json_response = json.loads(response)
 
-        for result in json_response['d']['results']:
+        #Convert the string response to a Python dictionary object
+        xml_response = xml.loads(response)
+
+        for result in xml_response['d']['results']:
             results.append({
                 'title': result['Title'],
                 'link': result['Url'],
