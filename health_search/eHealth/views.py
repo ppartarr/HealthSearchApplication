6dce4276b91185abf4252c71707a9d2ce85d5ae4
aratherstrangeman <thechrisharrisproject@live.co.uk>
2016-03-11 19:05:56 +0000

    testing User changes

--- health_search/eHealth/views.py
+++ health_search/eHealth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from search.federated_search import federated_run_querys
+from eHealth.forms import UserForm, UserProfileForm
 
 
 def index(request):
@@ -26,7 +27,40 @@ def search(request):
     return render(request, 'eHealth/search.html', result_list)
 
 
-#todo implement
-def user(request):
-    response = render(request, 'eHealth/index.html')
-    return response
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+            'eHealth/register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
+
