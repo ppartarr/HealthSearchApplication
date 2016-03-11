ef3e7a3afcd2abc451034ba5ce6b2329f3de7776
2142915 <ppartarr@outlook.com>
2016-03-11 16:17:24 +0000

    med+_search.py

--- health_search/search/medlineplus_search.py
+++ health_search/search/medlineplus_search.py
@@ -1,13 +1,32 @@
-#todo do
-
-import xml
+import xml, json
 import urllib, urllib2
-
 
 if __name__ == '__main__':
     main()
 
 
 def medlineplus_run_query(search_terms):
+    root_url = "https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term="
+    query = search_terms.replace(" ", "+")
+    search_url = "{0}{1}".format(
+        root_url,
+        search_url
+    )
+
     results=[]
+    print search_url
+    try:
+        response = urllib2.urlopen(search_url).read()
+        json_response = json.loads(response)
+
+        for result in json_response['d']['results']:
+            results.append({
+                'title': result['Title'],
+                'link': result['Url'],
+                'summary': result['Description'],
+                'from': 'Bing.com',
+            })
+    except urllib2.URLError as e:
+        print "Error when querying the Medline+ API: ", e
+
     return results
