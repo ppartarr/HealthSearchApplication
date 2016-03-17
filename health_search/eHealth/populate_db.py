ffbf12ea288f0f573f2282595d972ef341e2fe1c
ppartarr <ppartarr@orange.fr>
2016-03-17 18:39:30 +0000

    added some users in the population script

--- health_search/eHealth/populate_db.py
+++ health_search/eHealth/populate_db.py
@@ -7,3 +7,19 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
+    add_user(name="Bob",
+             gender_choices='male',
+             dateOfBirth="1978-06-23")
+
+    add_user(name="Jen",
+             gender_choices='female',
+             dateOfBirth="1982-01-01")
+
+    add_user(name="Jill",
+             gender_choices='female',
+             dateOfBirth="1956-09-01")
+
+def add_user(name):
+    u = UserProfile.objects.get_or_create()[0]
+    u.save()
+    return u
