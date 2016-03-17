24bf06bde09e57c5016fd4ffffc268c3f30ccbb0
ppartarr <ppartarr@orange.fr>
2016-03-17 19:19:09 +0000

    some more corrections in population script

--- health_search/populate_db.py
+++ health_search/populate_db.py
@@ -1,6 +1,6 @@
 __author__ = 'Philippe'
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','eHealth.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_search.settings')
 
 import django
 django.setup()
@@ -10,21 +10,23 @@ from eHealth.models import UserProfile, Category, Page
 def populate():
     add_user(name="Bob",
              gender_choices='male',
-             dateOfBirth="1978-06-23")
+             dob="1978-06-23")
 
     add_user(name="Jen",
              gender_choices='female',
-             dateOfBirth="1982-01-01")
+             dob="1982-01-01")
 
     add_user(name="Jill",
              gender_choices='female',
-             dateOfBirth="1956-09-01")
+             dob="1956-09-01")
 
-def add_user(name):
-    u = UserProfile.objects.get_or_create()[0]
+
+def add_user(name, gender_choices, dob):
+    u = UserProfile.objects.get_or_create(name=name, gender=gender_choices, dateOfBirth=dob)[0]
     u.save()
     return u
 
+
 if __name__ == '__main__':
-    print "Starting the population script"
+    print "Starting the population script..."
     populate()
