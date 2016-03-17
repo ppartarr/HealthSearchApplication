c9dd9b382c74d85b4ef3508dddc87903521fce91
thekevinchi <thekevinchi@gmail.com>
2016-03-17 22:10:59 +0000

    added catorgys and pages to useres (from TWD)

--- health_search/eHealth/forms.py
+++ health_search/eHealth/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from eHealth.models import UserProfile
+from eHealth.models import UserProfile, Page, Category
 from django.forms import extras
 from django.core import validators
 
@@ -30,3 +30,35 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('dateOfBirth', 'gender','picture')
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # An inline class to provide additional information on the form.
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Category
+        fields = ('name',)
--- health_search/eHealth/views.py
+++ health_search/eHealth/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from search.federated_search import federated_run_querys
-from eHealth.forms import UserForm, UserProfileForm
+from eHealth.forms import UserForm, UserProfileForm, PageForm, CategoryForm
 from eHealth.models import Category,Page,UserProfile
 from django.contrib.auth import authenticate, login
 from django.core.context_processors import request
@@ -49,11 +49,11 @@ def user(request):
 
     user=UserProfile.objects.filter(user=request.user).get()
     context_dict = {}
-    context_dict['username']=request.user.get_username()
-    context_dict['email']   = request.user.email
-    context_dict['dob']     = user.dateOfBirth
-    context_dict['gender']  = user.gender
-    all_caegories=Category.objects.filter(user=user)
+    context_dict['username']        = request.user.get_username()
+    context_dict['email']           = request.user.email
+    context_dict['dob']             = user.dateOfBirth
+    context_dict['gender']          = user.gender
+    context_dict['all_caegories']   = Category.objects.filter(user=user)
     response = render(request, 'eHealth/user.html',default_context(context_dict))
     return response
 
@@ -104,3 +104,78 @@ def register(request):
             'registration/register.html',
             default_context({'user_form': user_form, 'profile_form': profile_form, 'registered': registered}))
 
+def category(request, category_name_slug):
+    # Create a context dictionary which we can pass to the template rendering engine.
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # So the .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        context_dict['category_name_slug'] = category.slug
+
+        # Retrieve all of the associated pages.
+        # Note that filter returns >= 1 model instance.
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message for us.
+        pass
+
+    # Go render the response and return it to the client.
+    return render(request, 'eHealth/category.html', context_dict)
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat}
+
+    return render(request, 'eHealth/add_page.html', context_dict)
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'eHealth/add_category.html', {'form': form})
