2b475ccb70c726c0dc43917595bdb824bf397bf9
ppartarr <ppartarr@orange.fr>
2016-03-17 18:44:28 +0000

    corrections in population script

--- health_search/eHealth/populate_db.py
+++ health_search/eHealth/populate_db.py
@@ -4,7 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','eHealth.settings')
 import django
 django.setup()
 
-from rango.models import Category, Page
+from eHealth.models import UserProfile, Category, Page
 
 def populate():
     add_user(name="Bob",
