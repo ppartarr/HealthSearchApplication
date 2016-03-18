83bf525bfdf53509fd2dcaa428eea545308d50e9
thekevinchi <thekevinchi@gmail.com>
2016-03-18 01:30:27 +0000

    added error checking to scores.py

--- health_search/scores/scores.py
+++ health_search/scores/scores.py
@@ -6,51 +6,61 @@ from django.utils import html
 
 ######todo finish
 
-#todo get url from db
-url="http://www.tangowithdjango.com/book17/chapters/new17.html"
+# todo get url from db
+url = "http://www.tangowithdjango.com/book17/chapters/new17.html"
+
 
 def _html_stripper(url):
     response = urllib2.urlopen(url)
     htmlresponce = response.read()
-    #replaces <br> and <p> tags with newline charecter
-    text=html.linebreaks(htmlresponce)
-    #strips remaing html tags
-    text=html.strip_tags(text)
+    # replaces <br> and <p> tags with newline charecter
+    text = html.linebreaks(htmlresponce)
+    # strips remaing html tags
+    text = html.strip_tags(text)
     return text
 
+
 def _get_readability_score(text):
-    readability_score=textstat.flesch_reading_ease(text)
-    return readability_score
+    try:
+        readability_score = textstat.flesch_reading_ease(text)
+        return readability_score
+    except:
+        print '_get_readability_score'
+        return 0
+
 
 def _get_polarity_score(text):
-    polarity_score = TextBlob.polarity(text)
-    #polarity is returns as a flot between (-1.0,1.0) this converts it to a int between (0,100)
-    polarity_score = int(50+(polarity_score*50))
-    return polarity_score
+    try:
+        polarity_score = TextBlob.polarity(text)
+        # polarity is returns as a flot between (-1.0,1.0) this converts it to a int between (0,100)
+        polarity_score = int(50 + (polarity_score * 50))
+        return polarity_score
+    except:
+        print '_get_polarity_score Error'
+        return 0
 
 
 def _get_subjectivity_score(text):
-    subjectivity_score = TextBlob.subjectivity(text)
-    #subjectivity is returns as a flot between (0.0,1.0) this converts it to a int between (0,100)
-    subjectivity_score = int(subjectivity_score*100)
+    try:
+        subjectivity_score = TextBlob.subjectivity(text)
+        # subjectivity is returns as a flot between (0.0,1.0) this converts it to a int between (0,100)
+        subjectivity_score = int(subjectivity_score * 100)
 
-    return subjectivity_score
-
+        return subjectivity_score
+    except:
+        print '_get_subjectivity_score Error'
+        return 0
 
 
 def get_all_scores(url):
     try:
-        text=_html_stripper(url)
-
-        readability  = _get_readability_score(text)
-        polarity     = _get_polarity_score(text)
-        subjectivity = _get_subjectivity_score(text)
-
-        return (readability,polarity,subjectivity)
+        text = _html_stripper(url)
     except:
         print 'error reading page'
-        return(0,0,0)
-#todo add score to db
+        return (0, 0, 0)  # todo add score to db
 
+    readability = _get_readability_score(text)
+    polarity = _get_polarity_score(text)
+    subjectivity = _get_subjectivity_score(text)
 
-
+    return (readability, polarity, subjectivity)
